62d6c6ebf25c4df0d13cf29ea28556d3d52ec174
Pannio <1821299140@qq.com>
2019-07-16 16:36:51 +0800

    添加vue-router,vuex组件

---
+++ src/components/Counter.vue
@@ -0,0 +1,16 @@
+<template>
+  <cube-button v-on:click="count++" >
+      You clicked me {{ count }} times.
+  </cube-button>
+</template>
+
+<script>
+export default {
+  name: 'Counter',
+  data: function () {
+    return {
+      count: 0
+    }
+  }
+}
+</script>
